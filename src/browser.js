ac41bdb17afa7841504d38b02e799784acc74c15
Simon de Haan <simon@praekeltfoundation.org>
2016-04-11 05:20:36 -0700

    split out go.states

--- src/browser.js
+++ src/browser.js
@@ -2,97 +2,11 @@
 go.app = function() {
     var vumigo = require('vumigo_v02');
     var _ = require('lodash');
-    var Q = require('q');
     var App = vumigo.App;
     var Choice = vumigo.states.Choice;
     var EndState = vumigo.states.EndState;
-    var PaginatedState = vumigo.states.PaginatedState;
-    var PaginatedChoiceState = vumigo.states.PaginatedChoiceState;
-
-    var MessengerChoiceState = PaginatedChoiceState.extend(function(self, name, opts) {
-
-        opts = _.defaults(opts || {}, {
-            helper_metadata: function () {
-                // just disable for now
-                if(true)
-                    return {};
-
-                if (opts.choices.length > 3) {
-                    return {};
-                }
-
-                var i18n = self.im.user.i18n;
-                subtitle = i18n(opts.question);
-                buttons = opts.choices.map(function(choice, index) {
-                    return {
-                        title: i18n(choice.label),
-                        payload: {
-                            content: (index + 1) + '',
-                            in_reply_to: self.im.msg.message_id || null,
-                        }
-                    };
-                });
-
-                return {
-                    messenger: {
-                        template_type: 'generic',
-                        title: i18n(opts.title),
-                        subtitle: subtitle,
-                        image_url: opts.image_url || '',
-                        buttons: buttons
-                    }
-                };
-            }
-        });
-
-        PaginatedChoiceState.call(self, name, opts);
-
-    });
-
-    var MessengerPaginatedState = PaginatedState.extend(function (self, name, opts) {
-
-        opts = _.defaults(opts || {}, {
-            helper_metadata: function () {
-                // just disable for now
-                if(true)
-                    return {};
-
-                var i18n = self.im.user.i18n;
-                var i = self.metadata.page;
-                var text = i18n(self.text);
-                var choices = _.mapValues(self.choices, i18n);
-                var n = self._chars(choices);
-
-                return Q
-                    .all([
-                        self.page(i, text, n),
-                        self.page(i + 1, text, n)])
-                    .spread(function(text, more) {
-                        return self._determine_choices(i < 1, !more);
-                    })
-                    .then(function (choices) {
-                        return {
-                            messenger: {
-                                template_type: 'generic',
-                                title: i18n(opts.title),
-                                subtitle: i18n(opts.text),
-                                image_url: opts.image_url || '',
-                                buttons: choices.map(function(choice_name, index) {
-                                    return {
-                                        title: i18n(self.choices[choice_name]),
-                                        payload: {
-                                            content: '' + (index + 1),
-                                            in_reply_to: self.im.msg.message_id || null,
-                                        }
-                                    };
-                                })
-                            }
-                        };
-                    });
-            }
-        });
-        PaginatedState.call(self, name, opts);
-    });
+    var MessengerPaginatedState = go.states.MessengerPaginatedState;
+    var MessengerChoiceState = go.states.MessengerChoiceState;
 
     var GoFAQBrowser = App.extend(function(self) {
         App.call(self, 'states_start');
